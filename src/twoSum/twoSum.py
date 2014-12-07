cbeff120cf66f8821823f94f2f3f58c1d037b754
simonclouds <simonclouds@gmail.com>
2014-12-07 09:39:08 +0800

    Update twoSum.py
    Accepted!

--- src/twoSum/twoSum.py
+++ src/twoSum/twoSum.py
@@ -1,18 +1,14 @@
 class Solution:
     # @return a tuple, (index1, index2)
     def twoSum(self, num, target):
-        letarget = len(num) -1;
+        m = {}
+        firstIndex = 1;
+        secondeIndex = 1;
         
-        while num[letarget] > target:
-            letarget = letarget - 1;
-        
-        for i in range(0,letarget):
-            firstnum = num[i]
-            diff = target - firstnum;
-            
-            secondnumlast = letarget;
-            while num[secondnumlast] > diff:
-                secondnumlast = secondnumlast-1;
-            for j in range(i+1, secondnumlast+1):
-                if num[i] + num[j] == target:
-                    return (i+1,j+1);
+        for i in range(0,len(num)):
+            if(m.get(num[i]) == None):
+                m[target - num[i]] = i;
+            else:
+                firstIndex = m.get(num[i]) + 1
+                secondIndex = i + 1
+                return (firstIndex,secondIndex);
